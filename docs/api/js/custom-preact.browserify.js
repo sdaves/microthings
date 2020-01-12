53a5c6ad356f50224c63dfd2b95f28ddf90f3a2b
Stephen Daves <contact@stephendaves.com>
2020-01-11 22:44:04 -0500

    Update custom-preact.browserify.js

--- docs/api/js/custom-preact.browserify.js
+++ docs/api/js/custom-preact.browserify.js
@@ -1,10 +1,10 @@
 window.CustomHtml = window.CustomHtml || {};
 
 var preact = require('preact');
-for (var i in preact) { window.CustomHtml[i] = preact[i]; }
+for (var i in preact) { window.CustomHtml[i] = function() { return preact[i].apply(preact, arguments) } }
 
 var proppy = require('proppy');
-for (var i in proppy) { window.CustomHtml[i] = proppy[i]; }
+for (var i in proppy) { window.CustomHtml[i] = function() { return proppy[i].apply(proppy, arguments) } }
 
 var proppyPreact = require('proppy-preact');
-for (var i in proppyPreact) { window.CustomHtml[i] = proppyPreact[i]; }
+for (var i in proppyPreact) { window.CustomHtml[i] = function() { return proppyPreact[i].apply(proppyPreact, arguments) } }
